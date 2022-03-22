c3a1d3a16dca4722fd49532e70ece1f2ec37233f
Danny Warren <danny@theforbiddenarts.com>
2022-03-22 13:56:04 -0400

    Add comment to net6 code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// This is how you use the Console.WriteLine() method
+// Also note that you can use the "cw" snippet to create
+// the Console.WriteLine() method signiture.
 Console.WriteLine("Hello, World!");
+Console.WriteLine("How are you?");
+
+Console.ReadLine();
